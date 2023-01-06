ad846a96bff4986e5dcaf7b98a2d5fd24355f499
ManideepMuddagowniM <muddagownimanideep@gmail.com>
2023-01-06 16:45:12 +0000

    setup pckg

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+# create a function to install the requirements.txt file
+
+def get_requirements()->list[str]:
+
+    '''
+    This function returns the list of requirements
+    '''
+
+    requirement_list:list[str]=[]
+    return requirement_list
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Manideep Muddagowni',
+    author_email='muddagownimanideep@gmail.com',
+    packages='find_packages',
+    install_requires=get_requirements(),
+    )
+
