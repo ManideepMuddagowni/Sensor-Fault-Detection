337f8ac1de4988a85260162fa8803187aa6ae91f
ManideepMuddagowniM <muddagownimanideep@gmail.com>
2023-01-07 18:33:28 +0000

    database

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 
 # create a function to install the requirements.txt file
 
-def get_requirements()->list[str]:
+def get_requirements()->List[str]:
 
     '''
     This function returns the list of requirements
     '''
 
-    requirement_list:list[str]=[]
+    requirement_list:List[str]=[]
     return requirement_list
+    
 setup(
-    name='sensor',
-    version='0.0.1',
-    author='Manideep Muddagowni',
-    author_email='muddagownimanideep@gmail.com',
-    packages='find_packages',
-    install_requires=get_requirements(),
+        name='sensor',
+        version='0.0.1',
+        author='Manideep Muddagowni',
+        author_email='muddagownimanideep@gmail.com',
+        packages=find_packages(),
+        install_requires=get_requirements(),
     )
 
