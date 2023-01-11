e7e9391f9cb9963102fbb3f17313b670acdf041c
ManideepMuddagowniM <muddagownimanideep@gmail.com>
2023-01-11 11:09:20 +0000

    app.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os
+
+def test_exception():
+    try:
+        x=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
+
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+
+    
+    ## mongodb_client = MongoDBClient()
+    ## print(mongodb_client.database.list_collection_names())
