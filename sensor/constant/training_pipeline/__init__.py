a863f742cec9adc09d14831942f2bfed32ca33eb
ManideepMuddagowniM <muddagownimanideep.g@gmail.com>
2023-01-20 13:15:33 +0000

    re

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -6,7 +6,7 @@ TARGET_COLUMN = 'class'
 PIPELINE_NAME: str = 'sensor'
 ARTIFACT_DIR: str = 'artifact'
 
-# common file name 
+# common file name for training pipeline
 
 FILE_NAME: str = 'sensor.csv'
 
@@ -19,9 +19,27 @@ MODEL_NAME = 'model.pkl'
 SCHEMA_FILE_PATH = os.path.join('congig', 'schema.yaml')
 
 ## we will drop unwanted data using config folder with schema.yaml file 
-# will call drop_columns in schema.yaml file
+## will call drop_columns in schema.yaml file
 
-SCHEMA_DROP_COLS = "drop_columns"
+SCHEMA_DROP_COL = "drop_columns"
 
 ## Data Ingestion related constant start with DATA_INGESTION VAR NAME
+
+'''
+Import the data from the mongo db with the collection name
+'''
+DATA_INGESTION_COLLECTION_NAME: str = 'sensor'
+'''
+Export that data from the mongo db to feature store
+'''
+DATA_INGESTION_DIR_NAME: str = 'data_ingestion'
+
+DATA_INGESTION_FEATURE_STORE: str = 'feature_store'
+
+'''
+Dividing the data into train test split and store it in the ingested folder
+'''
+DATA_INGESTION_INGESTED_DIR: str = 'ingested'
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = '0.2'
+
 
